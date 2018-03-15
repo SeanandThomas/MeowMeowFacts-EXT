436ca664bb1bb1f19c8e0bd0b73e5df9b3505b9d
Thomas Gorczynski <me@thomasgorczynski.com>
2018-03-15 00:07:47 -0500

    i think it is done

--- main.js
+++ main.js
@@ -17,10 +17,12 @@ _gaq.push(["_trackPageview"]);
 var background_colors = ["#E24223", "#2B2B77", "#D3B298"];
 var cat_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var kitty, bgc;
-bgc = background_colors[Math.floor(Math.random() * background_colors.length)];
+bgc = background_colors[
+  Math.floor(Math.random() * background_colors.length)
+].substring(1);
 
 $(document).ready(function() {
-  $(`body`).css(`background-color`, `${bgc}`);
+  $(`body`).css(`background-color`, `#${bgc}`);
 
   $(`button`).click(function() {
     var $temp = $("<input>");
