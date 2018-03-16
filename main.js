48efd4874d1225658c4d64e2ff1e1d3a3673efee
Thomas Gorczynski <me@thomasgorczynski.com>
2018-03-15 20:21:34 -0500

    updating things

--- main.js
+++ main.js
@@ -15,7 +15,29 @@ _gaq.push(["_trackPageview"]);
 })();
 
 var background_colors = ["#E24223", "#2B2B77", "#D3B298"];
-var cat_num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+var cat_num = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20
+];
 var kitty, bgc;
 bgc = background_colors[
   Math.floor(Math.random() * background_colors.length)
